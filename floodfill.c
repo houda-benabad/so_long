09205135da56208c1cf7ff674cd6945aee88a272
hobenaba <116219944+hobenaba@users.noreply.github.com>
2023-01-06 17:01:33 +0100

    Add files via upload

--- floodfill.c
+++ floodfill.c
@@ -6,7 +6,7 @@
 /*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 10:51:34 by hobenaba          #+#    #+#             */
-/*   Updated: 2023/01/06 12:33:23 by hobenaba         ###   ########.fr       */
+/*   Updated: 2023/01/06 17:00:41 by hobenaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,8 @@ char	**floodfill_map(t_LIST *game)
 	return (p);
 }
 
-int	floodfill_map_changer(char **flmap, int j, int i)
+void	floodfill_map_changer(char **flmap, int j, int i)
 {
-	//printf("%c\n", flmap[j][i]);
 	if (flmap[j][i + 1] != '1' && flmap[j][i + 1] != 'E'
 		&& flmap[j][i + 1] != 'H')
 	{
@@ -58,7 +57,28 @@ int	floodfill_map_changer(char **flmap, int j, int i)
 		flmap[j - 1][i] = 'H';
 		floodfill_map_changer(flmap, j - 1, i);
 	}
-	return (0);
+}
+
+int	ft_check_themap(char **p, int line, int chara)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 0;
+	if (p[line][chara] == 'C')
+		a = 5;
+	else if (p[line][chara] == 'E')
+	{
+		if (p[line][chara + 1] != 'H' && p[line][chara - 1] != 'H'
+			&& p[line + 1][chara] != 'H' && p[line - 1][chara] != 'H')
+		b = 6;
+	}
+	else if (a == 6 && b == 5)
+		return (7);
+	else if (a != 0)
+		return (b);
+	return (a);
 }
 
 int	floodfill(t_LIST *game)
@@ -66,43 +86,31 @@ int	floodfill(t_LIST *game)
 	char	**p;
 	int		j;
 	int		i;
-	int		rq;
-	int		rq2;
 
-	p = floodfill_map(game);
+	j = 0;
+	i = 0;
+	p = floodfill_map (game);
 	ft_find_player(game);
-	j = game -> p_line;
-	i = game -> p_chara;
-	rq = 0;
-	rq2 = 0;
-	floodfill_map_changer(p, j, i);
+	//floodfill_map_changer(p, game -> p_line, game -> p_chara);
 	printf("the first line : %s\n", p[0]);
 	printf("the second line : %s\n", p[1]);
 	printf("the third line : %s\n", p[2]);
 	printf("the fourth line : %s\n", p[3]);
 	printf("the fifth line : %s\n", p[4]);
 	printf("the sixth line : %s\n", p[5]);
-	j = 0;
 	while (p[j])
 	{
 		i = 0;
 		while (p[j][i])
 		{
-			if (p[j][i] == 'C')
-				rq = 5;
-			else if (p[j][i] == 'E')
+			if (p[j][i] == 'C' || p[j][i] == 'E')
 			{
-				if (p[j][i + 1] != 'H' && p[j][i - 1] != 'H'
-					&& p[j + 1][i] != 'H' && p[j - 1][i] != 'H')
-					rq2 = 6;
+				if (ft_check_themap(p, j, i) != 0)
+					return (ft_check_themap(p, j, i));
 			}
 			i++;
 		}
 		j++;
 	}
-	if (rq == 5 && rq2 == 6)
-		return (7);
-	else if (rq == 0)
-		return (rq2);
-	return (rq);
+	return (0);
 }
